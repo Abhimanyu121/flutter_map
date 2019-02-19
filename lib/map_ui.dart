f1ff3c00feecb4c0a60aba4694972d9b454792e7
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2019-02-19 20:46:17 +0530

    where everything is messed up

--- lib/map_ui.dart
+++ lib/map_ui.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'LocationServices.dart';
+//import 'LocationServices.dart';
 
 class Mapui extends StatefulWidget{
   Mapui(Map mp){
